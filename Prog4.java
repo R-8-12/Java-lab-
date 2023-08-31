77f5094c341093e3f08c6b80f8c4d7569dbd9fd3
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-08-31 13:04:17 +0530

    Update Prog4.java

--- Prog4.java
+++ Prog4.java
@@ -2,7 +2,7 @@
 //Name:-Aditya Sharma
 //Class- CSIT-1
 //Roll No.-12
-//1.Print an integer entered by the user.
+//4.Even/Odd
 import java.util.*;
 class Prog4{
     public static void main(String[] args){
