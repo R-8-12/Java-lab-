70e36515d82ccb6c5dda25869f158bc54f3311eb
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-08-31 15:56:16 +0530

    Add files via upload

---
+++ Prog14.java
@@ -0,0 +1,13 @@
+//Name :-Aditya Sharma
+//Roll No.-12
+//Class:-CSIT-1
+
+public class Prog14{
+    int x=5;
+    public static void main(String[] args) {
+        Prog14 obj=new Prog14();
+        System.out.print("The value assigned to instance variable is "+obj.x);
+    }
+}
+    
+
