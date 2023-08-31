21a6965c13575a65c5d5e43c000224530ffa67ee
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-08-31 13:05:35 +0530

    Update Prog6.java

--- Prog6.java
+++ Prog6.java
@@ -2,7 +2,7 @@
 //Name:-Aditya Sharma
 //Class- CSIT-1
 //Roll No.-12
-//1.Print an integer entered by the user.
+//6.Negative/Positive
 import java.util.*;
 public class Prog6{
     public static void main(String[] args) {
