40dc7f49e61e0abf560ce74cad4f5b9838f5ccdf
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-08-31 12:32:58 +0530

    Add files via upload

---
+++ Prog3.java
@@ -0,0 +1,21 @@
+
+//Name:-Aditya Sharma
+//Class- CSIT-1
+//Roll No.-12
+//1.Print an integer entered by the user.
+import java.util.*;
+class Prog3{
+    public static void main(String[] args){
+        System.out.print("Enter two numbers:   ");
+        Scanner s=new Scanner(System.in);
+        int n1=s.nextInt();
+        System.out.println("Our 1st number is "+n1);
+        int n2=s.nextInt();
+        System.out.println("Our 2nd number is "+n2);
+        int a;
+        a=n1;
+        n1=n2;
+        n2=a;
+        System.out.println("The number after swapping are \nThe first number "+n1+"\nThe second number "+n2);
+    }
+}
