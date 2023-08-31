9c92a0deafe75b9718a646f0b6cf413bbc3fd05a
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-08-31 13:49:44 +0530

    Add files via upload

---
+++ Prog10.java
@@ -0,0 +1,12 @@
+//Name :-Aditya Sharma
+//Roll No.-12
+//Class:-CSIT-1
+import java.util.*;
+public class Prog10{
+    public static void main(String[] args) {
+        for(char i='A';i<='Z';i++){
+            System.out.print(i+"    ");
+        }
+    }
+    
+}
