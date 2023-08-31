d15795d8d9cd7980b240994c78d8640863e5f70d
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-08-31 12:24:13 +0530

    Add files via upload

---
+++ Prog1.java
@@ -0,0 +1,14 @@
+
+//Name:-Aditya Sharma
+//Class- CSIT-1
+//Roll No.-12
+//1.Print an integer entered by the user.
+import java.util.*;
+class Prog1{
+    public static void main(String[] args){
+        System.out.print("Enter any number:   ");
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.println("The number entered by the user is "+n);
+    }
+}
