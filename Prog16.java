95bed16a1a22d5912edad1b7f17436368b905a43
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-08-31 16:09:27 +0530

    Add files via upload

---
+++ Prog16.java
@@ -0,0 +1,19 @@
+//Name :-Aditya Sharma
+//Roll No.-12
+//Class:-CSIT-1
+
+public class Prog16{ 
+    static int x;
+    Prog16(int i){ //setter
+        x=i;
+    }
+    public static void get(){ //Getter
+        System.out.println(x);
+    }
+    public static void main(String[] args) {
+        Prog16 obj=new Prog16(100);
+        Prog16.get();
+    }
+}
+    
+
