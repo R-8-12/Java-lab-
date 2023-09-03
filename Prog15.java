b76dbea47ac24f04eda319c08045805748d1bbed
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-09-03 09:38:28 -0700

    Update Prog15.java

--- Prog15.java
+++ Prog15.java
@@ -3,9 +3,9 @@
 //Class:-CSIT-1
 
 public class Prog15{ 
-    static int x;
+    private static int x;
     Prog15(int i){ //setter
-        x=i;
+        this.x=i;
     }
     public static void get(){ //Getter
         System.out.println(x);
