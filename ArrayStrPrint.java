9f3938ad277ba71ab5320c13933c5406d44de3d5
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-09-26 12:58:37 +0530

    Update ArrayStrPrint.java

--- ArrayStrPrint.java
+++ ArrayStrPrint.java
@@ -1,11 +1,6 @@
 //WAP to printmin and max element of the given array.
-import java.util.Arrays;
 public class ArrayStrPrint{
 	public static void main(String[] args){
-	   /* int n;
-	    Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the range of an array:    ");
-	    n=sc.nextInt(); */
 	    int s=0;
 	    int a[]={12,45,67,34};
 	    System.out.println("Integer array is"+Arrays.toString(a)+" ");
