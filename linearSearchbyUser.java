569971b66c04cc9515b6f80c7c776dc2a1e67781
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-10-03 12:18:45 +0530

    Update linearSearchbyUser.java

--- linearSearchbyUser.java
+++ linearSearchbyUser.java
@@ -15,12 +15,13 @@ public static int linearsearch(int key, int []a){
         Scanner sc=new Scanner(System.in);
 	    System.out.print("Enter the range of an array:    ");
 	    n=sc.nextInt();
-	    System.out.println("Enter the value to be seached:  ");
-	    val=sc.nextInt();
 	    int a1[]=new int[n];
+	    System.out.println("Enter the elements in array:    ");
 	    for(int i=0;i<n;i++){
 	        a1[i]=sc.nextInt();
 	    }
+	    System.out.print("Enter the value to be seached:  ");
+	    val=sc.nextInt();
 	    System.out.println(val+" is found at index: "+linearsearch(val,a1));
     }
 }
