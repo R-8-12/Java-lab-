c6d3cda967a0c43b0583728099e0e629ea25dc4c
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-10-05 12:13:42 +0530

    Update TwoDStrArrdemonstration.java

--- TwoDStrArrdemonstration.java
+++ TwoDStrArrdemonstration.java
@@ -1,30 +1,4 @@
-/*import java.util.*;
-public class TwoDStrArr
-{
-	public static void main(String[] args) {
-	    Scanner sc=new Scanner(System.in);
-	    int m,n;
-	    System.out.print("Enter the rows of an array:    ");
-	    m=sc.nextInt();
-	    System.out.print("Enter the columns of an array:    ");
-	    n=sc.nextInt(); 
-	    String s[][]=new String[m][n];
-	    System.out.print("Enter String:   ");
-	    for(int i=0;i<m;i++){
-	        for(int j=0;j<n;j++){
-	            s[i][j]=sc.next();
-	        }
-	        System.out.println();
-	    }
-	    for(String []k:s){
-	        for(String val:k){
-	            	System.out.print(val+"\t");
-	        }
-	        System.out.println();
-	    }
-	}
-}*/
-public class TwoDStrArrdemonstration
+ class TwoDStrArrdemonstration
 {
 	public static void main(String[] args){
 	    String[][] salutation={{"Mr.","Mrs.","Ms."},{"Kumar"}};
