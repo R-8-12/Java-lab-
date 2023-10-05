dd10cd8b37245478c89bc8f3007b5db5f3506fd9
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-10-05 12:02:42 +0530

    Add files via upload

---
+++ TwoDStrArr.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class TwoDStrArr
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int m,n;
+	    System.out.print("Enter the rows of an array:    ");
+	    m=sc.nextInt();
+	    System.out.print("Enter the columns of an array:    ");
+	    n=sc.nextInt(); 
+	    String s[][]=new String[m][n];
+	    System.out.print("Enter String:   ");
+	    for(int i=0;i<2;i++){
+	        for(int j=0;j<2;j++){
+	            s[i][j]=sc.next();
+	        }
+	        System.out.println();
+	    }
+	    for(String []k:s){
+	        for(String val:k){
+	            	System.out.print(val+"\t");
+	        }
+	        System.out.println();
+	    }
+	    
+	
+	}
+}
