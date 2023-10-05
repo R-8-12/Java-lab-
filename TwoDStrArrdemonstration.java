354be85794d5a45d3d8838b3c2f5333204632146
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-10-05 12:11:24 +0530

    Add files via upload

---
+++ TwoDStrArrdemonstration.java
@@ -0,0 +1,34 @@
+/*import java.util.*;
+public class TwoDStrArr
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int m,n;
+	    System.out.print("Enter the rows of an array:    ");
+	    m=sc.nextInt();
+	    System.out.print("Enter the columns of an array:    ");
+	    n=sc.nextInt(); 
+	    String s[][]=new String[m][n];
+	    System.out.print("Enter String:   ");
+	    for(int i=0;i<m;i++){
+	        for(int j=0;j<n;j++){
+	            s[i][j]=sc.next();
+	        }
+	        System.out.println();
+	    }
+	    for(String []k:s){
+	        for(String val:k){
+	            	System.out.print(val+"\t");
+	        }
+	        System.out.println();
+	    }
+	}
+}*/
+public class TwoDStrArrdemonstration
+{
+	public static void main(String[] args){
+	    String[][] salutation={{"Mr.","Mrs.","Ms."},{"Kumar"}};
+	     System.out.println(salutation[0][0]+" "+salutation[1][0]);
+	      System.out.println(salutation[0][1]+salutation[1][0]);
+	} 
+}
