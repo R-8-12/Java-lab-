b0b30921a45e999be8080dbae63bb0f67056d6a8
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-10-10 12:25:38 +0530

    Add files via upload

---
+++ LabradorSuperAndOverwritingMethod.java
@@ -0,0 +1,21 @@
+class Animal{
+    public void eat(){
+        System.out.println("I can eat");
+    }
+}
+class Dog extends Animal{
+    public void eat(){
+        super.eat();
+        System.out.println("I eat dog food");
+    }
+    public void bark(){
+        System.out.println("I can bark");
+    }
+}
+class LabradorSuperAndOverwritingMethod{
+    public static void main(String[] args){
+        Dog labrador=new Dog();
+        labrador.eat();
+        labrador.bark();
+    }
+}
