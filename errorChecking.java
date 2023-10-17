9390fdff5a2203fa12765bd8f319024eeb1b078a
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-10-17 12:02:45 +0530

    Add files via upload

---
+++ errorChecking.java
@@ -0,0 +1,15 @@
+//package errorhandling;
+public class errorChecking
+{
+	public static void main(String[] args) {
+    try{
+        int x=10;
+        int y=0;
+        int z=x/y;
+        System.out.println(z);
+    }
+    catch(Exception err){
+        System.out.println("....."+err);
+    }
+}
+}
