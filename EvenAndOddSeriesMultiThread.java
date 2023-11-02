700394698b9a40cbfce2ea5fb9269cab04aa4195
R-8-12 <143330167+R-8-12@users.noreply.github.com>
2023-11-02 12:45:30 +0530

    Add files via upload

---
+++ EvenAndOddSeriesMultiThread.java
@@ -0,0 +1,40 @@
+class Print{
+    public void print(int n1,int n2,int n3){
+        if(n3==0){
+            for(int i=n1;i<=n2;i++){
+                if(i%2==0){
+            System.out.print(i+"\t");
+        }
+        }
+        System.out.println();
+        }
+        else if(n3==1){
+            for(int i=n1;i<=n2;i++){
+                if(i%2!=0){
+            System.out.print(i+"\t");
+        }
+            }
+        }
+    }
+}
+class Thread1 extends Thread{
+    Print t1=new Print();
+    public void run(){
+        t1.print(1,50,0);
+    }
+}
+class Thread2 extends Thread{
+    Print t2=new Print();
+    public void run(){
+        t2.print(1,50,1);
+    }
+}
+public class EvenAndOddSeriesMultiThread{
+	public static void main(String[] args) {
+		Thread1 s1=new Thread1();
+		Thread2 s2=new Thread2();
+		s1.start();
+		s2.start();
+	}
+}
+
